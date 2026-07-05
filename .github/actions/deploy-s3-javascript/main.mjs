5977068d0fe7da042e7c2b891a22262c093bf910
thanhthanh0535 <thanhthanh0535@gmail.com>
2026-07-04 21:57:32 -0400

    upload to s3 aws

--- .github/actions/deploy-s3-javascript/main.mjs
+++ .github/actions/deploy-s3-javascript/main.mjs
@@ -1,8 +1,16 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+//import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
+    // input values
+    const bucket = core.getInput('bucket',{required: true});
+    const bucketregion = core.getInput('bucket-region',{required: true});
+    const distFolder = core.getInput('dist-folder',{required: true});
+
+    //upload file
+    const s3url = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3url} --region ${bucketregion} `)
     core.notice('Hello from my custom Javascript action!');
   
 }
